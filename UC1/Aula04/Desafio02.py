ba7e5d3c1d35967bbe371c3c80792c8b30b7970f
Juliane <julianedguerra@gmail.com>
2025-10-30 20:50:42 -0300

    Atualização de desafios

--- UC1/Aula04/Desafio02.py
+++ UC1/Aula04/Desafio02.py
@@ -3,3 +3,9 @@ largura = float(input('Largura?'))
 altura = float(input('Altura?'))
 
 cxazul = 1.5
+
+area = 2 * (comprimento + largura) * altura
+
+caixas = area / cxazul
+
+print(f'São necessárias {caixas} caixas.')
