580a06c5bd6fa2b03d470b972f8511f47ac1cfe5
JULIANE DUARTE GUERRA <juliane.guerra@aluno.senac.rio>
2025-11-03 21:49:22 -0300

    Att

--- UC1/Aula05/Desafio02.py
+++ UC1/Aula05/Desafio02.py
@@ -1,16 +1,39 @@
-contador = 0
-limite = 12
+# contador = 0
+# limite = 12
 
-while contador < limite:
+# while contador < limite:
+#     try:
+#         data_nascimento = int(input('Insira seu ano de nascimento'))
+#         if data_nascimento > 2007:
+#             print('Você não pode participar do programa')
+#             break
+#         else:
+#             input('Nome: ')
+#             input('Telefone: ')
+#             input('E-mail: ')
+#         contador = contador + 1
+#     except: 
+#         print('Valor inválido.')
+contador_candidatos = 12
+for candidato in range(contador_candidatos):
     try:
-        data_nascimento = int(input('Insira seu ano de nascimento'))
-        if data_nascimento > 2007:
-            print('Você não pode participar do programa')
+        ano_nascimento = int(input('Informe o ano de seu nascimento: '))
+        ano_atual = 2025
+        idade = ano_atual - ano_nascimento
+
+        if idade < 18:
+            print('Menores de 18 anos não podem participar do programa.')
             break
+        elif idade >= 18:
+            telefone =  input('Telefone: ')
+            email = input('Email: ')
+            contador_candidatos -= 1
+            print('-'*40)
+            print(f'Candidatura recebida. Obrigado! Sua candidatura:\n{telefone},\n{email}')
+            print('-'*40)
         else:
-            input('Nome: ')
-            input('Telefone: ')
-            input('E-mail: ')
-        contador = contador + 1
-    except: 
-        print('Valor inválido.')
+            print('Informe um ano de nascimento válido.')
+    except ValueError:
+        print('ERRO: informe dados válidos.')
+    except SyntaxError:
+        print('ERRO: informe dados válidos.')
