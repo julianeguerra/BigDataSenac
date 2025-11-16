b4863a403e45059460ac53f11e855f21ece94515
Juliane <julianedguerra@gmail.com>
2025-11-15 22:16:06 -0300

    Desafios

--- UC1/Aula08/Desafio04.py
+++ UC1/Aula08/Desafio04.py
@@ -1,3 +1,9 @@
+'''
+Regras do ano bissexto: É divisível por 4, exceto para anos divisíveis por 100, a
+menos que sejam também divisíveis por 400. (Ex: 2000 e 2400 são bissextos; 1900
+e 2100 não são)
+'''
+
 def eh_bissexto(ano):
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         return True
@@ -7,13 +13,11 @@ def eh_bissexto(ano):
 try:
     ano_usuario = int(input('Insira um ano:'))
     ano = eh_bissexto(ano_usuario)
+    
+    if ano: # Se 'resultado' for True
+        print(f"O ano {ano_usuario} É bissexto.")
+    else: # Se 'resultado' for False
+        print(f"O ano {ano_usuario} NÃO é bissexto.")
 except ValueError:
     print('ERRO: insira um ano váldido.')      
-
-eh_bissexto(ano)
-
-'''
-Regras do ano bissexto: É divisível por 4, exceto para anos divisíveis por 100, a
-menos que sejam também divisíveis por 400. (Ex: 2000 e 2400 são bissextos; 1900
-e 2100 não são)
-'''
+    
