c36857b31bb006e00e09e98a28306379d1633403
JULIANE DUARTE GUERRA <juliane.guerra@aluno.senac.rio>
2025-12-03 21:46:38 -0300

    Aula07/Projeto

--- UC2/Projeto/Projeto.py
+++ UC2/Projeto/Projeto.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import numpy as np
+import mysql.connector
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('MostStreamedSpotifySongs2024.csv', encoding='latin1', sep=',')
-print(df.head(10))
+df = pd.read_csv('track_data_final.csv', encoding='utf-8', sep=',')
+print(df.head())
+df.info()
+df.describe()
