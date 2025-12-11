93e6e06746ed2f96f3013d8c74a7b664f9d9f778
JULIANE DUARTE GUERRA <juliane.guerra@aluno.senac.rio>
2025-12-10 21:36:05 -0300

    Aula 10/Projeto

--- UC2/Projeto/Projeto.py
+++ UC2/Projeto/Projeto.py
@@ -1,10 +1,57 @@
-import pandas as pd
-import numpy as np
-import mysql.connector
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import pandas as pd
+# import numpy as np
+# import mysql.connector
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
-df = pd.read_csv('track_data_final.csv', encoding='utf-8', sep=',')
-print(df.head())
-df.info()
-df.describe()
+# df = pd.read_csv('track_data_final.csv', encoding='utf-8', sep=',')
+# print(df.head())
+# df.info()
+# df.describe()
+
+
+# import pandas as pd
+# import numpy as np
+# import mysql.connector
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import mysql.connector
+
+# df = pd.read_csv('high_popularity_spotify_data', encoding='utf-8', sep=',')
+# # print(df.head())
+# # df.info()
+# # df.describe()
+
+# for linha in df["album_release_date"]:
+#     if len(linha) > 4:
+#         pass
+#     else:
+#         df["album_release_date"].add("-01-01")
+
+# print(df["album_release_date"].head(30))
+
+import mysql.connector
+
+# 1. Conectar ao banco de dados
+conexao = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="",
+    database="spotify"
+)
+
+cursor = conexao.cursor()
+
+query_popularidade = "SELECT track_name, track_popularity, track_artist FROM high_popularity_spotify_data WHERE track_popularity > 50"
+
+cursor.execute(query_popularidade)
+
+resultados_popularidade = cursor.fetchall()
+
+# 6. Exibir os resultados
+for linha in resultados_popularidade:
+    print(linha)
+
+# 7. Fechar a conexão
+cursor.close()
+conexao.close()
--- UC2/Projeto/high_popularity_spotify_data.sql
+++ UC2/Projeto/high_popularity_spotify_data.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE spotify;
 USE spotify;
+SET SESSION sql_mode = '';
+
 
 CREATE TABLE high_popularity_spotify_data (
-    energy FLOAT,
+    energy INT,
     tempo FLOAT,
     danceability FLOAT,
     playlist_genre VARCHAR(50),
@@ -18,27 +20,32 @@ CREATE TABLE high_popularity_spotify_data (
     track_album_name VARCHAR(500),
     playlist_name VARCHAR(500),
     analysis_url VARCHAR(500),
-    track_id VARCHAR(255) PRIMARY KEY,
+    track_id VARCHAR(255),
     track_name VARCHAR(500),
-    track_album_release_date DATE,
-    instrumentalness FLOAT,
+    track_album_release_date VARCHAR(100),
+    instrumentalness DECIMAL,
     track_album_id VARCHAR(255),
     music_mode INT,
     music_key INT,
     duration_ms INT,
-    acousticness FLOAT,
-    id VARCHAR(255),
+    acousticness DECIMAL,
+    id VARCHAR(255) PRIMARY KEY,
     playlist_subgenre VARCHAR(100),
     music_type VARCHAR(100),
     playlist_id VARCHAR(255)
 );
 
+DESCRIBE high_popularity_spotify_data;
+
 LOAD DATA LOCAL INFILE "C:/Users/juliane.guerra/OneDrive - SENAC RIO/Documentos/BigDataSenac/UC2/Projeto/high_popularity_spotify_data.csv" -- Ajuste o caminho no seu banco local
 INTO TABLE spotify.high_popularity_spotify_data
-FIELDS TERMINATED BY ',' 
+FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n' -- Aqui: CR LF
 IGNORE 1 ROWS -- Pula o cabeçalho 'id_produto,nome...'
-(energy,tempo,danceability,playlist_genre,loudness,liveness,valence,track_artist,time_signature,speechiness,track_popularity,track_href,uri,track_album_name,playlist_name,analysis_url,track_id,track_name,track_album_release_date,instrumentalness,track_album_id,music_mode,music_key,duration_ms,acousticness,id,playlist_subgenre,music_type,playlist_id) -- Mapeia colunas
+(energy,tempo,danceability,playlist_genre,loudness,liveness,valence,track_artist,time_signature,speechiness,track_popularity,track_href,uri,track_album_name,playlist_name,analysis_url,track_id,track_name,track_album_release_date,instrumentalness,track_album_id,music_mode,music_key,duration_ms,acousticness,id,playlist_subgenre,music_type,playlist_id); -- Mapeia colunas
 
 SELECT * FROM high_popularity_spotify_data;
+
+SELECT COUNT(*) FROM high_popularity_spotify_data;
+
